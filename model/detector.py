02a62b9acd47738a03259ae3ae95d3f0b0d79430
Admir <admir@qyobo.com>
2025-08-11 15:49:51 +0200

    Resolve errors

--- model/detector.py
+++ model/detector.py
@@ -104,7 +104,7 @@ class Decoder(nn.Module):
         Test forward pass for the decoder. Applies Mel + GriffinLim and extracts the watermark from the audio.
         """
         spect, phase = stft(
-            x, n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length
+            x.squeeze(1), n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length
         )
         extracted_wm = self.extractor(spect.unsqueeze(1)).squeeze(1)
         msg = torch.mean(extracted_wm,dim=2, keepdim=True).transpose(1,2)
